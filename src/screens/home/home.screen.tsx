c67817926aa9e1da2ffe03a347ef948030c70398
Raymond Bertram <raymond.bertram@gmx.de>
2024-06-26 13:40:00 +0200

    saving progress due to lags

---
+++ src/screens/home/home.screen.tsx
@@ -0,0 +1,17 @@
+import { firaCode } from "@/app/fonts";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <p className={`${firaCode.className} py-2`}>Hi, my name is</p>
+      <h1 className="text-4xl not-italic py-2">Raymond Bertram</h1>
+      <h2 className="text-2xl py-2">Full Stack Software Engineer</h2>
+      <p className={`${firaCode.className} text-base py-2`}>
+        I am a software engineer specializing on full stack web and mobile app
+        development. Currently I am focused on building innovative and
+        sustainable technologies to enhance peoples lives.
+      </p>
+      <button className="flex w-fit my-8 p-2 bg-black text-base text-[#FFFFFF] rounded">Check Out my CV!</button>
+    </div>
+  );
+}
