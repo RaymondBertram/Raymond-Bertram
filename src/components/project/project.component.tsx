663e63f8d6bcd65753dbc95b53bf4dd9134ef892
Raymond Bertram <raymond.bertram@gmx.de>
2024-08-18 18:37:42 +0200

    main-page-refactoring-and-responsive-design

--- src/components/project/project.component.tsx
+++ src/components/project/project.component.tsx
@@ -5,28 +5,26 @@ import HoverButton from "../hoverButton/hoverButton.component";
 import pictureUnderline from "../../../public/images/picture-underline.png";
 import { IProject } from "@/types";
 
-import "./project.component.css";
-
 export default function Project({ title, skill1, skill2, skill3, description, img, screenId }:IProject ) {
   return (
-    <div className="lg:container lg:max-w-[1300px]">
-      <div className="project grid lg:py-35 items-center lg:justify-between lg:grid-cols-1 gap-x-[60px] gap-y-[14px]">
-        <div className="left-column w-full max-w-none min-w-[auto] lg:max-w-[50%] lg:min-w-[410px]">
+    <div className="py-10 lg:max-w-[1300px]">
+      <div className="project grid grid-cols-[auto] grid-rows-[auto] gap-y-4 gap-x-4  md:grid-cols-1 md:grid-rows-3 lg:py-35 lg:justify-between lg:grid-cols-1">
+        <div className="left-column row-start-1 col-start-1 row-end-1 col-end-2 w-full max-w-none min-w-[auto] md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-2 lg:max-w-[50%] lg:min-w-[410px]">
           <h4 className="text-[#b7b4b9] uppercase tracking-wide mt-0 lg:mb-5 text-sm font-bold leading-[110%]">latest work</h4>
-          <h1 className="mt-0 mb-5 font-['Abril Fatface'] font-normal leading-[140%] text-[37px] lg:text-4xl">{title}</h1>
+          <h1 className="mt-0 pr-2 mb-5 font-['Abril Fatface'] font-normal leading-[140%] text-[37px] lg:text-[64px]">{title}</h1>
           <div className="tags-wrapper flex flex-wrap mb-8 box-border">
             <SkillButton skill={skill1} />
             <SkillButton skill={skill2} />
             <SkillButton skill={skill3} />
           </div>
         </div>
-        <a href="" className="row-start-2 col-start-1 row-end-3 col-end-3 lg:row-start-1 lg:col-start-2 lg:row-end-3 lg:col-end-3 self-center justify-self-end">
-          <motion.div whileHover={{ scale: 1.1, x: 30, y: -15 }}>
-            <Image className="mock-up-img z-10 relative max-w-full align-middle	inline-block" src={img} alt="underline" width={520} height={300}/>
+        <a href="#" className="object-fill row-start-2 row-end col-start-1 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-2  lg:col-end-3 self-center lg:justify-self-end">
+          <motion.div className="" whileHover={{ scale: 1.1, x: 30, y: -15 }}>
+            <Image className="mock-up-img z-10 relative align-middle inline-block w-full md:w-3/6 lg:w-[500px]" src={img} alt="underline" width={250} height={370}/>
           </motion.div>
-          <Image className="dots relative top-auto bottom-40 lg:right-8" src={pictureUnderline}  alt="Project" width={101} height={100} />
+          <Image className="dots hidden relative top-auto bottom-[168px] -left-4 lg:block lg:right-8" src={pictureUnderline}  alt="Project" width={100} height={100} />
         </a>
-        <div className="content-block row-start-3 row-end-4 col-end-3 lg:row-start-2 col-start-1 lg:row-end-3  self-start">
+        <div className="content-block row-start-[auto] row-end-[auto] col-end-1 self-center lg:row-start-2 col-start-1 lg:row-end-3">
           <p className="portfolio-paragraph lg:max-w-[480px] text-lg leading-8 mb-[10px]">{description}</p>
           <HoverButton text="see this project" screenId={screenId}/>
         </div>
